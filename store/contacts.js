41db10e736f0eb948536445bc08be906c939dec8
Luis Orlando Bustamante <orlandobustamantep@gmail.com>
2018-05-16 12:12:40 -0600

    contacts store

---
+++ store/contacts.js
@@ -0,0 +1,14 @@
+export const state = () => ({
+    loadedContacts: ['Orlando', 'Amelia']
+})
+    
+
+// export const getters = {
+//     contactsCount: state => {
+//         return state.loadedContacts.length
+//     }
+// }
+
+// export const mutations = {}
+// export const actions = {}
+
