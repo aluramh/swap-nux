ca3e89e1b990204bada3725bc49b061ea6bd4cce
Luis Orlando Bustamante <orlandobustamantep@gmail.com>
2018-05-16 12:12:13 -0600

    added all contacts page

---
+++ pages/contacts/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>All contacts</h1>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Main</h1>
+  <b-row>
+    <b-col cols md="4" sm="6" xs="12" v-for="contact in contacts" :key="contact">
+      <Card :contact="contact"/>
+    </b-col>
+  </b-row>
 </template>
+
+
+<script>
+import Card from '@/components/cards/Card.vue'
+export default {
+  asyncData() {
+    return {
+      contacts: ['Orlando']
+    }
+  },
+  components: {
+    Card
+  }
+}
+</script>
