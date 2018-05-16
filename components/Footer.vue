69cd6a77e069ae4e7d9ae40d21f76d10ceb30a90
Luis Orlando Bustamante <orlandobustamantep@gmail.com>
2018-05-16 12:10:18 -0600

    added footer

---
+++ components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>
+        © Property of Jonajo Consulting LLC
+    </p>
+</template>
+
+<style scoped>
+    p {
+        text-align: center;
+        margin: 1rem auto;
+    }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,21 +5,30 @@
         <b-row>
           <b-col cols="2"></b-col>
           <b-col>
-            <nuxt/>
+            <b-container class="main-container">
+              <nuxt/>
+            </b-container>
           </b-col>
         </b-row>
-      <b-row></b-row>
     </b-container>
-
+    <Footer/>
   </div>
 </template>
 
 
 <script>
 import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
+
+<style scoped>
+.main-container{
+  padding: 1rem;
+}
+</style>
