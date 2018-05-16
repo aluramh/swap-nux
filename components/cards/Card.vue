536c669dec0b60022e24a57afb64f51020c6cada
Luis Orlando Bustamante <orlandobustamantep@gmail.com>
2018-05-16 12:10:08 -0600

    Added Card component

---
+++ components/cards/Card.vue
@@ -0,0 +1,29 @@
+<template>
+    <b-card class="swap-card"> 
+        <div class="card-text card-bodt">
+            {{ contact }}
+        </div>
+    </b-card>
+    
+</template>
+
+<script>
+export default {
+    props: ['contact']
+}
+</script>
+
+<style scoped>
+.swap-card{
+    transition: 0.8s;
+    height: 12.5rem;
+    max-height: 15rem;
+    margin: 0 auto;
+    font-size: 80%;
+}
+.card-body{
+    max-height: 15rem;
+    background: linear-gradient(to bottom,rgba(222, 222, 222, 0),rgba(222, 222, 222, 1));
+}
+</style>
+
