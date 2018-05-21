244f6d9779cdcfb58dd3ab90d4fdd770626a9d04
Luis Orlando Bustamante <orlandobustamantep@gmail.com>
2018-05-21 17:47:35 -0600

    Renamed components

--- components/Header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <b-navbar type="light" variant="light" >
-            <b-container>
-                <b-navbar-brand>Swap Demo</b-navbar-brand>
-            </b-container>
-        </b-navbar>
-    </div>
-</template>
---
+++ components/TheSidebar.vue
@@ -0,0 +1,24 @@
+<template>
+        <b-col cols="2" class="sidebar">
+            <b-row class="justify-content-center">
+                <b-button-group vertical>
+                    <b-button variant="link" @click="hideFilters">Hide Filters</b-button>
+                    <b-button variant="link">Clear All</b-button>
+                </b-button-group>
+            </b-row>
+            
+        </b-col>
+        
+
+</template>
+
+
+<script>
+export default {
+    methods: {
+        hideFilters() {
+            this.$emit('hidden')
+        }
+    }
+}
+</script>
