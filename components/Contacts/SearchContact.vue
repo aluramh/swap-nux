229eb251e146da20b83383dbd69fcd0c5c340f69
Luis Orlando Bustamante <orlandobustamantep@gmail.com>
2018-05-21 17:49:09 -0600

    added search component

---
+++ components/Contacts/SearchContact.vue
@@ -0,0 +1,42 @@
+<template>
+       <div>
+           <v-search
+            :list="list"
+            option-value="id"
+            :custom-text="nameAndPosition"
+            v-model="selectedContact"
+            placeholder="Search contact by name"
+            @searchchange="searchAction"
+           />
+       </div>
+
+</template>
+
+
+<script>
+export default {
+    data () {
+        return {
+            selectedContact: {},
+        }
+    },
+    props: {
+        searchAction: Function
+    },
+    methods: {
+        nameAndPosition(item) {
+            return `${item.name} - ${item.position}`
+        }
+    },
+    computed: {
+        list() {
+            return this.$store.state.loadedContacts
+        }
+    },
+    watch: {
+        selectedContact(contact) {
+            this.$router.push('/contacts/' + contact.id)
+        }
+    }
+}
+</script>
