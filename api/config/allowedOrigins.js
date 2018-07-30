cb8665852484b58fc3339d987c72e9d2ff26a3a9
Alex R <aluramh@gmail.com>
2018-07-30 16:33:22 -0500

    Allowed origins

---
+++ api/config/allowedOrigins.js
@@ -0,0 +1,19 @@
+// This file can be use to whitelist accepted origins from where requests
+// are allowed to come from.
+
+const port = process.env.PORT || 3000;
+const allowedOrigins = [`http://localhost:${port}`, `http://127.0.0.1:${port}`];
+
+module.exports = (req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  next();
+};
