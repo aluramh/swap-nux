494c827c29dd78b5f69288f0dc2fe9db7f1d2fad
aluramh <aluramh@gmail.com>
2018-07-31 11:47:08 -0500

    added dotenv at the start of the server.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-require("dotenv").config();
 
 router.use("/contacts", require("./contacts"));
 router.use("/user", require("./user"));
